df8527419ab6eebc5aaf6e169a333f0fb9746dd2
Antipov <antpv99@gmail.com>
2020-12-03 16:03:01 +0200

    Configuring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,26 @@
 const path = require('path')
-const webpack = require('webpack')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-const NODE_ENV = process.env.NODE_ENV || 'production'
-const isDev = NODE_ENV === 'development'
-const isProd = NODE_ENV === 'production'
-
-const webpackConfig = {
-  context: path.join(__dirname, 'src'),
-
-  entry: {
-    'src': ['webpack-dev-server/client', 'webpack/hot/dev-server', './index.js']
-  },
-
+module.exports = {
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/build'),
-    publicPath: '/build/',
-    filename: 'build.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    library: 'contenteditableMaxLength',
+    libraryTarget:'umd'
   },
-
-  mode: NODE_ENV,
-
-  devtool: isDev ? 'source-map' : false,
-
-  watch: false,
-
-  plugins: [
-    new webpack.EnvironmentPlugin(['NODE_ENV']),
-    new webpack.DefinePlugin({
-      __DEV__: JSON.stringify(isDev)
-    }),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-
-  resolve: {
-    modules: ['node_modules'],
-    extensions: ['.js', '.json']
-  },
-
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: [
-              '@babel/preset-env'
-            ]
+            presets: ['@babel/preset-env']
           }
         }
       }
     ]
   },
-
-  devServer: {
-    port: 9000,
-    contentBase: __dirname,
-    publicPath: '/build/',
-    hot: true
-  }
+  mode: 'production'
 }
-
-if (isProd) {
-  webpackConfig.optimization = {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          warnings: false,
-          parse: {},
-          compress: {},
-          mangle: true,
-          output: {
-            comments: false,
-          },
-          toplevel: false,
-          nameCache: null,
-          ie8: false,
-          keep_fnames: false
-        }
-      })
-    ]
-  }
-}
-
-module.exports = webpackConfig
